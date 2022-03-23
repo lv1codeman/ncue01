48e996f95c2a98224e81dbbb67a7b20e70340f6b
lv1codeman <lv1codeman@gmail.com>
2022-03-23 16:34:46 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// Load plugins
+
+const browsersync = require("browser-sync").create();
+const gulp = require("gulp");
+
+// BrowserSync
+function browserSync(done) {
+    browsersync.init({
+        server: {
+            baseDir: "./"
+        },
+        port: 3000
+    });
+    done();
+}
+
+// BrowserSync reload
+function browserSyncReload(done) {
+    browsersync.reload();
+    done();
+}
+
+// Watch files
+function watchFiles() {
+    gulp.watch("./*.html", browserSyncReload);
+}
+
+// Define complex tasks
+// const watch = gulp.series(gulp.parallel(watchFiles, browserSync));
+const watch = gulp.parallel(watchFiles, browserSync);
+
+// Export tasks
+exports.watch = watch;
